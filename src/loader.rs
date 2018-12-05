748e37086d58f25e046cdad6747b8c89e0f00b82
Lilith645 <lilith@inet-sys.com>
2018-12-06 00:24:36 +1000

    Should fix travis ci fail Changes committed: 	modified:   src/loader.rs

--- src/loader.rs
+++ src/loader.rs
@@ -254,19 +254,3 @@ impl fmt::Display for LoadingError {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use instance::loader::DynamicLibraryLoader;
-    use instance::loader::LoadingError;
-
-    #[test]
-    fn dl_open_error() {
-        unsafe {
-            match DynamicLibraryLoader::new("_non_existing_library.void") {
-                Err(LoadingError::LibraryLoadFailure(_)) => (),
-                _ => panic!(),
-            }
-        }
-    }
-}
-
