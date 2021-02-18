d26a63f4aa81fa301fc11615b0652685a992efe9
Lilith645 <lilith@inet-sys.com>
2021-02-18 10:41:55 +1000

    Removing maat-vulkan for rebase Changes to be committed: 	modified:   .gitmodules 	deleted:    src/camera/mod.rs 	deleted:    src/camera/ortho_camera.rs 	deleted:    src/camera/perspective_camera.rs 	deleted:    src/core.rs 	deleted:    src/drawcalls.rs 	deleted:    src/font.rs 	deleted:    src/generate_terrain.rs 	deleted:    src/gltf_interpreter.rs 	deleted:    src/graphics.rs 	deleted:    src/helperfunctions/mod.rs 	modified:   src/lib.rs 	deleted:    src/math.rs 	deleted:    src/model_data.rs 	deleted:    src/resource_manager.rs 	deleted:    src/shaders/compute/boids.comp 	deleted:    src/shaders/compute/sharpen.comp 	deleted:    src/shaders/final_shader.rs 	deleted:    src/shaders/glsl/VkFinal.frag 	deleted:    src/shaders/glsl/VkFinal.vert 	deleted:    src/shaders/glsl/VkImGui.frag 	deleted:    src/shaders/glsl/VkImGui.vert 	deleted:    src/shaders/glsl/VkModel.frag 	deleted:    src/shaders/glsl/VkModel.vert 	deleted:    src/shaders/glsl/VkModelDeffered.frag 	deleted:    src/shaders/glsl/VkModelDeffered.vert 	deleted:    src/shaders/glsl/VkModelInstanced.vert 	deleted:    src/shaders/glsl/VkText.frag 	deleted:    src/shaders/glsl/VkText.vert 	deleted:    src/shaders/glsl/VkTextInstanced.vert 	deleted:    src/shaders/glsl/VkTexture.frag 	deleted:    src/shaders/glsl/VkTexture.vert 	deleted:    src/shaders/glsl/VkTextureInstanced.vert 	deleted:    src/shaders/glslangValidator 	deleted:    src/shaders/mod.rs 	deleted:    src/shaders/model_shader.rs 	deleted:    src/shaders/sprv/ComputeSharpen.spv 	deleted:    src/shaders/sprv/VkFinalFrag.spv 	deleted:    src/shaders/sprv/VkFinalVert.spv 	deleted:    src/shaders/sprv/VkImGuiFrag.spv 	deleted:    src/shaders/sprv/VkImGuiVert.spv 	deleted:    src/shaders/sprv/VkModelDefferedFrag.spv 	deleted:    src/shaders/sprv/VkModelDefferedVert.spv 	deleted:    src/shaders/sprv/VkModelFrag.spv 	deleted:    src/shaders/sprv/VkModelInstancedVert (copy).spv 	deleted:    src/shaders/sprv/VkModelInstancedVert.spv 	deleted:    src/shaders/sprv/VkModelVert.spv 	deleted:    src/shaders/sprv/VkTextFrag.spv 	deleted:    src/shaders/sprv/VkTextInstancedVert.spv 	deleted:    src/shaders/sprv/VkTextVert.spv 	deleted:    src/shaders/sprv/VkTextureFrag.spv 	deleted:    src/shaders/sprv/VkTextureInstancedVert.spv 	deleted:    src/shaders/sprv/VkTextureVert.spv 	deleted:    src/shaders/texture_shader.rs 	deleted:    src/threadpool.rs

--- src/camera/mod.rs
+++
@@ -1,5 +0,0 @@
-pub use self::ortho_camera::OrthoCamera;
-pub use self::perspective_camera::PerspectiveCamera;
-pub use self::perspective_camera::Direction as PerspectiveCameraDirection;
-mod ortho_camera;
-mod perspective_camera;
--- src/shaders/mod.rs
+++
@@ -1,9 +0,0 @@
-pub use self::texture_shader::TextureShader;
-pub use self::model_shader::ModelShader;
-pub use self::final_shader::FinalShader;
-pub use self::final_shader::FinalVertex;
-
-#[macro_use]
-mod texture_shader;
-mod model_shader;
-mod final_shader;
